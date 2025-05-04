d0c827826e7b47d740a9cc3eadab2b98e559e2e3
ticonlinepm <ajamini6631@gmail.com>
2025-05-04 23:11:36 +0330

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import Login from "./components/Login";
+
 function App() {
   return (
-    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', padding: '2rem' }}>
-      <h1>پروژه Ticonlinepm</h1>
-      <p>نسخه آزمایشی با Vite و React آماده است.</p>
+    <div>
+      <Login />
     </div>
   );
 }
