3d75e610b2fa01b77db91a2590f1575e88af2349
ticonlinepm <ajamini6631@gmail.com>
2025-05-04 22:06:22 +0330

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,9 @@
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-export default {
-  plugins: [react()]
-};
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist'
+  }
+});
